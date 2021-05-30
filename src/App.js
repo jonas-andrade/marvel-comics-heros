0255842b9d3bc00960784509ce74c410a26751a7
jonas-andrade <jonas6116m@outlook.com>
2021-05-30 10:49:10 -0300

    teste da api-marvel obs: codigo sem key privada por mutivos dde segurança

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import Main from './components/Main.js';
+import React,{ useEffect } from 'react';
+import axios from  'axios'
+import md5 from 'md5';
+ 
+const apikey='c21f06176bd264935b18146deb090a49'
 
+const ts = Number(new Date())
+const hash = md5( ts + privkey + apikey)
+const baseUrl = 'http://gateway.marvel.com/v1/public/characters'
 
-export default function App(){
-		return(
-			<>
-			<Main />
-			</>
-			)
-		}
+export default function App () {
+  useEffect(()=>{
+    axios.get(`${baseUrl}?ts=${ts}&apikey=${apikey}&hash=${hash}`)
+    .then(res => console.log(res.data))
+    .catch(e => console.error(e))},[])
+  
+    return (  
+             <>
+             {
+
+             
+             }
+             <h1>Testando API da Marvel ...</h1>
+             </>
+           )
+
+} //App
+
 
